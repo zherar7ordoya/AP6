d1be1a576abd9211ab41ff48970160a9e03c2db3
Gerardo Tordoya <zherar7ordoya@outlook.com>
2022-12-24 13:06:56 -0300

    2022-12-24

---
+++ TRABAJO FINAL/WellArchitectedApplication/src/CustomerFactory/Factory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.Configuration;
+using System.Configuration;
+using Interfaces;
+using CustomerLibrary;
+using ValidationLogic;
+
+namespace Factory
+{
+    //Design pattern :- Simple Factory Pattern 
+    // To improve perfomance we use singelton pattern 
+    // Follow IOC principle using Dependency Injection via Unity 
+    // Make this class Generic so that the logic is decoupled from the data type.
+
+    public static class Factory<INJECTTYPE>
+    {
+        static IUnityContainer oUnitCont = null;
+       
+        public static INJECTTYPE Create(string CustType) // will return whatever type is injected
+        {
+            if (oUnitCont==null)
+            {
+                if (ConfigurationManager.GetSection("unity") != null)
+                {
+                    oUnitCont = new UnityContainer();
+                    //Here we are registering different customer types and injecting the validation methods accordingly
+                    //Or We can load the entities to be injected from the configuration file
+                    // oUnitCont.LoadConfiguration();
+
+                    oUnitCont.RegisterType<CustomerBase, Visitor>("Visitor", new InjectionConstructor(new VisitorValidation()));
+                    oUnitCont.RegisterType<CustomerBase, Customer>("Customer", new InjectionConstructor(new CustomerValidation()));                                    
+
+                    
+                }
+            }
+            return oUnitCont.Resolve<INJECTTYPE>(CustType.ToString()); //resolves to any type injected
+
+        }
+        
+    }
+}
---
+++ TRABAJO FINAL/WellArchitectedApplication/src/EFDALLibrary/EFDal.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using InterfacesDAL;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFDALLibrary
+{
+    public class EFDalAbstract<INJECTTYPE> : IRepository<INJECTTYPE>
+        where INJECTTYPE:class /// it is telling the that the type we are injecting is a class
+    {
+
+        DbContext dbcont = null; // this Dbcontext will come from UOW
+        public EFDalAbstract()
+        {
+            dbcont = new EfUoW(); // If client doesn't call UOI it will initiated self contained transaction without commit and rollback
+        }
+
+        public void Add(INJECTTYPE obj)
+        {
+            /// Using the Entitytframework Set<>.Add that can be used to save the entity of type INJECTTYPE.          
+            /// This only places the objects in memory ready for commit
+            dbcont.Set<INJECTTYPE>().Add(obj);
+        }
+
+        public void Save()
+        {
+            dbcont.SaveChanges(); // This is for the physical committ to the database. 
+            
+        }
+
+        public List<INJECTTYPE> Search()
+        {
+            //Return the collection of Injected Type objects via Entity Framework
+            return dbcont.Set<INJECTTYPE>()
+                .AsQueryable<INJECTTYPE>()
+                .ToList<INJECTTYPE>();
+        }
+
+        public void SetUnitWOrk(IUow uow)
+        {
+            dbcont = ((EfUoW)uow); // Global Transaction comes into action 
+        }
+
+        public void Update(INJECTTYPE obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
