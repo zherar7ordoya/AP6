710346c3358fea9c5141f38b321390aff8232970
Gerardo Tordoya <zherar7ordoya@outlook.com>
2023-04-05 12:51:59 -0300

    2023-04-05

--- [ASO] Romero/Semana 02 - Intro a Procesos/Programación/alo.c
+++ [ASO] Romero/Semana 02 - Intro a Procesos/Programación/alo.c
@@ -1,6 +1,7 @@
-/** START FOR DUMMIES
+/**
  * COMPILAR: gcc alo.c -o alo
- * EJECUTAR: ./alo (Ubuntu) alo (Windows)
+ * EJECUTAR: ./alo (Ubuntu)
+ *             alo (Windows)
  */
 
 #include <stdio.h>
