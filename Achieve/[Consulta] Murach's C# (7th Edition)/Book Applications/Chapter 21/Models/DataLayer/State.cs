c06298e8d36f29cffdadd8a522c00cf6101a58d5
zherar7ordoya <zherar7ordoya@outlook.com>
2023-04-17 19:51:25 -0300

    2023-04-17

--- Achieve/[Consulta] Murach's C# (7th Edition)/Book Applications/Chapter 21/Models/DataLayer/State.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CustomerMaintenance.Models.DataLayer
-{
-    public class State
-    {
-        public string StateCode { get; set; }
-        public string StateName { get; set; }
-    }
-}
