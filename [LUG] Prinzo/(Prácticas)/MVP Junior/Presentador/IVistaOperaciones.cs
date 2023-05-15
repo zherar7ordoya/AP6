2439dff336eee9863fbd24a6ba08be99987d983f
Gerardo Rodolfo Tordoya <zherar7ordoya@outlook.com>
2023-05-15 03:36:02 -0300

    2023-05-15

---
+++ [LUG] Prinzo/(Prácticas)/MVP Junior/Presentador/IVistaOperaciones.cs
@@ -0,0 +1,9 @@
+﻿namespace Presentador
+{
+    public interface IVistaOperaciones
+    {
+        double Num1 { get; set; }
+        double Num2 { get; set; }
+        double Resultado { set; }
+    }
+}
