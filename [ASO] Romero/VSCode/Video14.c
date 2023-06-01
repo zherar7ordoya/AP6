2c77c262b41a21c18fc82758c88a979e38430f5a
zherar7ordoya <zherar7ordoya@outlook.com>
2023-06-01 12:53:13 -0300

    2023-06-01

---
+++ [ASO] Romero/VSCode/Video14.c
@@ -0,0 +1,14 @@
+
+/******************************************************************************\
+ *                                                                            *
+ *  Name: Gerardo Tordoya                                                     *
+ *                                                                            *
+ *  Description:                                                              *
+ *  Simulación de memoria contigua con particiones de igual tamaño.           *
+ *  https://youtu.be/qxP_ngz29wU                                              *
+ *                                                                            *
+ *  Created: 2023-06-01                                                       *
+ *  Updated: XXXX-XX-XX => ...                                                *
+ *                                                                            *
+ \*****************************************************************************/
+
