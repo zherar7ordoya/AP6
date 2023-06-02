191d12e824cdcab16bcc8892047b6110315707c7
zherar7ordoya <zherar7ordoya@outlook.com>
2023-06-02 13:11:22 -0300

    2023-06-02

---
+++ Xtras/Beginning C# Object-Oriented Programming (Dan Clark) 2nd Edition (2013)/Activity7_1/Activity7_1/Account.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity7_1
+{
+    abstract class Account
+    {
+        int _accountNumber;
+        protected double GetBalance(int accountNumber)
+        {
+            _accountNumber = accountNumber;
+            //Data normally retrieved from database.
+            if (_accountNumber == 1)
+            {
+                return 1000;
+            }
+            else if (_accountNumber == 2)
+            {
+                return 2000;
+            }
+            else
+            {
+                return -1; //Account number is incorrect
+            }
+        }
+    }
+    class CheckingAccount : Account
+    {
+    }
+    class SavingsAccount : Account
+    {
+        double _dblBalance;
+        public double Withdraw(int accountNumber, double amount)
+        {
+            _dblBalance = GetBalance(accountNumber);
+            if (_dblBalance >= amount)
+            {
+                _dblBalance -= amount;
+                return _dblBalance;
+            }
+            else
+            {
+                return -1; //Not enough funds
+            }
+        }
+    }
+}
---
+++ Xtras/Beginning C# Object-Oriented Programming (Dan Clark) 2nd Edition (2013)/Activity8_2/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Activity8_2
+{
+    public partial class frmLogger : Form
+    {
+        public frmLogger() { InitializeComponent(); }
+        private void btnLogInfo_Click(object sender, EventArgs e) { this.Text = Logger.LogWrite(txtLogPath.Text, txtLogInfo.Text); }
+        private void btnMessage_Click(object sender, EventArgs e) { MessageBox.Show("Hello"); }
+        private void btnSyncRead_Click(object sender, EventArgs e) { MessageBox.Show(Logger.LogRead(txtLogPath.Text)); }
+
+        private async void btnAsyncRead_Click(object sender, EventArgs e)
+        {
+            btnAsyncRead.Enabled = false;
+            string s = await Logger.LogReadAsync(txtLogPath.Text);
+            MessageBox.Show(s);
+            btnAsyncRead.Enabled = true;
+        }
+    }
+}
