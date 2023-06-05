936e32191ea29f0a92a5596cdf6931c5c19360d9
zherar7ordoya <zherar7ordoya@outlook.com>
2023-06-05 10:12:58 -0300

    2023-06-05

--- [LUG] Prinzo/ACQUIRE/LinQ/ChatGPTxmlCRUD/Program.cs
+++ [LUG] Prinzo/ACQUIRE/LinQ/ChatGPTxmlCRUD/Program.cs
@@ -6,6 +6,8 @@
  * correspondientes en el archivo XML.
  */
 
+using ChatGPTxmlCRUD.CRUD;
+
 using System;
 using System.Linq;
 using System.Xml.Linq;
@@ -41,16 +43,16 @@ namespace ChatGPTxmlCRUD
                 switch (option)
                 {
                     case "1":
-                        Muestra(ubicacion);
+                        ReadXml.Muestra(ubicacion);
                         break;
                     case "2":
-                        Agrega(ubicacion);
+                        CreateXml.Agrega(ubicacion);
                         break;
                     case "3":
-                        Actualiza(ubicacion);
+                        UpdateXml.Actualiza(ubicacion);
                         break;
                     case "4":
-                        Borra(ubicacion);
+                        DeleteXml.Borra(ubicacion);
                         break;
                     case "5":
                         Environment.Exit(0);
@@ -59,107 +61,6 @@ namespace ChatGPTxmlCRUD
                         Console.WriteLine("Opción no válida. Por favor, seleccione una opción válida.");
                         break;
                 }
-            }
-        }
-
-        static void Muestra(string ubicacion)
-        {
-            XDocument archivo = XDocument.Load(ubicacion);
-
-            Console.WriteLine("Datos:");
-
-            var personas = from persona in archivo.Descendants("Persona")
-                           select new
-                           {
-                               ID = persona.Attribute("id").Value,
-                               Nombre = persona.Element("Nombre").Value,
-                               Edad = persona.Element("Edad").Value
-                           };
-            
-            foreach (var persona in personas)
-            {
-                Console.WriteLine("ID: " + persona.ID);
-                Console.WriteLine("Nombre: " + persona.Nombre);
-                Console.WriteLine("Edad: " + persona.Edad);
-                Console.WriteLine("------------------------");
-            }
-        }
-
-        static void Agrega(string ubicacion)
-        {
-            XDocument archivo = XDocument.Load(ubicacion);
-
-            Console.WriteLine("Ingrese el ID:");
-            string id = Console.ReadLine();
-
-            Console.WriteLine("Ingrese el nombre:");
-            string nombre = Console.ReadLine();
-
-            Console.WriteLine("Ingrese la edad:");
-            string edad = Console.ReadLine();
-
-            XElement elemento = new XElement("Persona",
-                new XAttribute("id", id),
-                new XElement("Nombre", nombre),
-                new XElement("Edad", edad));
-
-            archivo.Root.Add(elemento);
-            archivo.Save(ubicacion);
-
-            Console.WriteLine("Dato agregado exitosamente.");
-        }
-
-        static void Actualiza(string ubicacion)
-        {
-            XDocument archivo = XDocument.Load(ubicacion);
-
-            Console.WriteLine("Ingrese el ID de la persona a actualizar:");
-            string id = Console.ReadLine();
-
-            XElement elemento = archivo.Root.Elements("Persona")
-                .FirstOrDefault(e => e.Attribute("id").Value == id);
-
-            if (elemento != null)
-            {
-                Console.WriteLine("Ingrese el nuevo nombre:");
-                string nombre = Console.ReadLine();
-
-                Console.WriteLine("Ingrese la nueva edad:");
-                string edad = Console.ReadLine();
-
-                elemento.Element("Nombre").Value = nombre;
-                elemento.Element("Edad").Value = edad;
-
-                archivo.Save(ubicacion);
-
-                Console.WriteLine("Dato actualizado exitosamente.");
-            }
-            else
-            {
-                Console.WriteLine("No se encontró la persona con el ID especificado.");
-            }
-        }
-
-        static void Borra(string ubicacion)
-        {
-            XDocument archivo = XDocument.Load(ubicacion);
-
-            Console.WriteLine("Ingrese el ID de la persona a eliminar:");
-            string id = Console.ReadLine();
-
-            XElement elemento = archivo.Root.Elements("Persona")
-                .FirstOrDefault(e => e.Attribute("id").Value == id);
-
-            if (elemento != null)
-            {
-                elemento.Remove();
-                archivo.Save(ubicacion);
-
-                Console.WriteLine("Dato eliminado exitosamente.");
-            }
-            else
-            {
-                Console.WriteLine("No se encontró la persona con el ID especificado.");
             }
         }
     }
